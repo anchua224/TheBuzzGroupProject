96c2ef2a7e3e19ae8907eb25aa9e1f34b044e791
JunchenBao <jub424@lehigh.edu>
2022-08-31 16:07:44 -0400

    added the skeleton of an admin app, using default maven archetype

---
+++ admin-cli/src/main/java/edu/lehigh/cse216/ju424/admin/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.ju424.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
