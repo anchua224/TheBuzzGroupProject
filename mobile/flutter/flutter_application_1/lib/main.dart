6ed58ec5fe714db38c3b5e9ce0efd8f73ac5aaa4
Junchen01 <jub424@lehigh.edu>
2022-09-12 15:48:52 -0400

    now uses dart object to marshall json data

--- mobile/flutter/flutter_application_1/lib/main.dart
+++ mobile/flutter/flutter_application_1/lib/main.dart
@@ -94,26 +94,31 @@ class HttpReqWords extends StatefulWidget {
 }
 
 class _HttpReqWordsState extends State<HttpReqWords> {
-  // Future<List<String>> _future_words = doSomeLongRunningCalculation();
-  late Future<List<String>> _future_words;
-  var _words = <String>['a', 'b', 'c', 'd'];
+  late Future<List<NumberWordPair>> _future_list_numword_pairs;
+
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   void initState() {
     super.initState();
-    _future_words = doSomeLongRunningCalculation();
+    _future_list_numword_pairs = fetchNumberWordPairs();
+  }
+
+  void _retry() {
+    setState(() {
+      _future_list_numword_pairs = fetchNumberWordPairs();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var fb = FutureBuilder<List<String>>(
-      future: _future_words,
-      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+    var fb = FutureBuilder<List<NumberWordPair>>(
+      future: _future_list_numword_pairs,
+      builder: (BuildContext context, AsyncSnapshot<List<NumberWordPair>> snapshot) {
         Widget child;
 
         if (snapshot.hasData) {
-          developer.log('`using` ${snapshot.data}', name: 'my.app.category');
+          // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
           // create  listview to show one row per array element of json response
           child = ListView.builder(
               //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
@@ -124,7 +129,8 @@ class _HttpReqWordsState extends State<HttpReqWords> {
                   children: <Widget>[
                     ListTile(
                       title: Text(
-                        snapshot.data![i],
+                        "row ${i}: num=${snapshot.data![i].num} str=${snapshot.data![i].str}",
+                        // snapshot.data![i].str,
                         style: _biggerFont,
                       ),
                     ),
@@ -132,10 +138,12 @@ class _HttpReqWordsState extends State<HttpReqWords> {
                   ],
                 );
               });
+        } else if (snapshot.hasError) { // newly added
+          child = Text('${snapshot.error}');
         } else {
           // awaiting snapshot data, return simple text widget
-          child = Text('Calculating answer...');
-          // child = const CircularProgressIndicator(); // could show a loading spinner.
+          // child = Text('Calculating answer...');
+          child = const CircularProgressIndicator(); //show a loading spinner.
         }
         return child;
       },
@@ -149,9 +157,9 @@ class _HttpReqWordsState extends State<HttpReqWords> {
 Future<List<String>> getWebData() async {
   developer.log('Making web request...');
    //var url = Uri.http('www.cse.lehigh.edu', '~spear/courses.json');
-  // var url = Uri.parse('http://www.cse.lehigh.edu/~spear/courses.json'); // list of strings
-  var url = Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json');      // list of maps
-  //var url = Uri.parse('https://jsonplaceholder.typicode.com/albums/1'); // single map
+  //var url = Uri.parse('http://www.cse.lehigh.edu/~spear/courses.json'); // list of strings
+  //var url = Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json');      // list of maps
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/albums/1'); // single map
   var headers = {"Accept": "application/json"};  // <String,String>{};
 
   var response = await http.get(url, headers: headers);
@@ -190,4 +198,51 @@ Future<List<String>> doSomeLongRunningCalculation() async {
 Future<List<String>> simpleLongRunningCalculation() async {
   await Future.delayed(Duration(seconds: 5)); // wait 5 sec
   return ['x', 'y', 'z'];
+}
+
+
+/// Create object from data like: http://www.cse.lehigh.edu/~spear/5k.json
+class NumberWordPair {
+  /// The string representation of the number
+  final String str; 
+  /// The int representation of the number
+  final int num;
+
+  const NumberWordPair({
+    required this.str,
+    required this.num,
+  });
+
+  factory NumberWordPair.fromJson(Map<String, dynamic> json) {
+    return NumberWordPair(
+      str: json['str'],
+      num: json['num'],
+    );
+  }
+}
+
+Future<List<NumberWordPair>> fetchNumberWordPairs() async {
+  final response = await http
+      .get(Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response, then parse the JSON.
+    final List<NumberWordPair> returnData;
+    var res = jsonDecode(response.body);
+    print('json decode: $res');
+
+    if( res is List ){
+      returnData = (res as List<dynamic>).map( (x) => NumberWordPair.fromJson(x) ).toList();
+    }else if( res is Map ){
+      returnData = <NumberWordPair>[NumberWordPair.fromJson(res as Map<String,dynamic>)];
+    }else{
+      developer.log('ERROR: Unexpected json response type (was not a List or Map).');
+      returnData = List.empty();
+    }
+    return returnData;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Did not receive success status code from request.');
+  }
 }
