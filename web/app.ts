68eeba8260c20f9869807bb6b2a5acb466c11054
JunchenBao <jub424@lehigh.edu>
2022-09-13 11:12:18 -0400

    Our typescript now displays a table of the messages

--- web/app.ts
+++ web/app.ts
@@ -101,9 +101,90 @@ class NewEntryForm {
     }
 } // end class NewEntryForm
 
+// a global for the main ElementList of the program.  See newEntryForm for 
+// explanation
+var mainList: ElementList;
+
+/**
+ * ElementList provides a way of seeing all of the data stored on the server.
+ */
+class ElementList {
+    /**
+     * refresh is the public method for updating messageList
+     */
+    refresh() {
+        // Issue an AJAX GET and then pass the result to update(). 
+        const doAjax = async () => {
+            await fetch('/messages', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            }).then( (response) => {
+                // If we get an "ok" message, clear the form
+                if (response.ok) {
+                    return Promise.resolve( response.json() );
+                }
+                // Otherwise, handle server errors with a detailed popup message
+                else{
+                    window.alert(`The server replied not ok: ${response.status}\n` + response.statusText);
+                }
+                return Promise.reject(response);
+            }).then( (data) => {
+                mainList.update(data);
+                console.log(data);
+            }).catch( (error) => {
+                console.warn('Something went wrong.', error);
+                window.alert("Unspecified error");
+            });
+        }
+
+        // make the AJAX post and output value or error message to console
+        doAjax().then(console.log).catch(console.log);
+    }
+
+    private update(data: any) {
+        let elem_messageList = document.getElementById("messageList");
+
+        if(elem_messageList !== null) {
+            elem_messageList.innerHTML = "";
+
+            let fragment = document.createDocumentFragment();
+            let table = document.createElement('table');
+
+            for (let i = 0; i < data.mData.length; ++i) {
+                let tr = document.createElement('tr');
+                let td_title = document.createElement('td');
+                let td_id = document.createElement('td');
+                td_title.innerHTML = data.mData[i].mTitle;
+                td_id.innerHTML = data.mData[i].mId;
+                tr.appendChild(td_id);
+                tr.appendChild(td_title);
+                tr.appendChild( this.buttons(data.mData[i].mId) );
+                table.appendChild(tr);
+            }
+            fragment.appendChild(table);
+
+            elem_messageList.appendChild(fragment);
+        }       
+    }
+
+    /**
+     * buttons() adds a 'delete' button to the HTML for each row
+     *
+     * doesn't do anything yet
+     */
+    private buttons(id: string): DocumentFragment {
+        return document.createDocumentFragment();
+    }
+} // end class ElementList
+
 // Run some configuration code when the web page loads
 document.addEventListener('DOMContentLoaded', () => {
-  // Create the object that controls the "New Entry" form
-  newEntryForm = new NewEntryForm();
-  window.alert('DOMContentLoaded');
+    // Create the object that controls the "New Entry" form
+    newEntryForm = new NewEntryForm();
+    // Create the object for the main data list, and populate it with data from the server
+    mainList = new ElementList();
+    mainList.refresh();
+    window.alert('DOMContentLoaded');
 }, false);
