5f4a60b395e06c2fe902852c324956e04e4af784
Alex <alg223@lehigh.edu>
2022-09-30 21:57:55 -0400

    sprint 4 for flutter

--- mobile/flutter/flutter_application_1/lib/main.dart
+++ mobile/flutter/flutter_application_1/lib/main.dart
@@ -1,19 +1,29 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'dart:developer' as developer;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+// This is going to be the post method that will post new ideas
+// that will go into the database
 void my_async_post_method() async {
   var data = {'title': 'My first post'};
   var resp = await http.post(
-    Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    Uri.parse(''),
     headers: {'Content-Type': 'application/json; charset=UTF-8'},
     body: json.encode(data),
   );
   print(resp.body);
+}
+
+// This is gping to be the get method that will get the ideas
+// from the database and display them in the app
+void my_async_get_method() async {
+  var url = Uri.https('');
+  var response = await http.get(url, headers: {});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 }
 
 void main() {
@@ -29,15 +39,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Buzz',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'The Buzz'),
@@ -76,11 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title), centerTitle: true,
       ),
       body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
         child: ListofIdeas(),
-        //child: HttpReqWords(),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
@@ -94,53 +91,43 @@ class ListofIdeas extends StatefulWidget {
 }
 
 class _ListofIdeasState extends State<ListofIdeas> {
-  late Future<List<NumberWordPair>> _future_list_numword_pairs;
-
-  final _biggerFont = const TextStyle(fontSize: 18);
-
   @override
   void initState() {
     super.initState();
-    _future_list_numword_pairs = fetchNumberWordPairs();
   }
 
   void _retry() {
-    setState(() {
-      _future_list_numword_pairs = fetchNumberWordPairs();
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    var fb = FutureBuilder<List<NumberWordPair>>(
-      future: _future_list_numword_pairs,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<NumberWordPair>> snapshot) {
+    var fb = FutureBuilder<List<Idea>>(
+      future: fetchIdeas(),
+      builder: (BuildContext context, AsyncSnapshot<List<Idea>> snapshot) {
         Widget child;
-
         if (snapshot.hasData) {
           // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
           // create  listview to show one row per array element of json response
           child = ListView.builder(
               //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(26.0),
               itemCount: snapshot.data!.length,
               itemBuilder: /*1*/ (context, i) {
                 return Column(
                   children: <Widget>[
                     ListTile(
                       title: Text(
-                        "row ${i}: num=${snapshot.data![i].num} str=${snapshot.data![i].str}",
-                        // snapshot.data![i].str,
-                        style: _biggerFont,
+                        snapshot.data![i].str,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
+                      tileColor: const Color.fromARGB(100, 181, 126, 220),
                     ),
-                    Divider(height: 1.0),
+                    const Divider(height: 8.0),
                   ],
                 );
               });
         } else if (snapshot.hasError) {
-          // newly added
           child = Text('${snapshot.error}');
         } else {
           // awaiting snapshot data, return simple text widget
@@ -150,99 +137,83 @@ class _ListofIdeasState extends State<ListofIdeas> {
         return child;
       },
     );
-
     return fb;
   }
 }
 
-Future<List<String>> getWebData() async {
-  developer.log('Making web request...');
-  //var url = Uri.http('www.cse.lehigh.edu', '~spear/courses.json');
-  //var url = Uri.parse('http://www.cse.lehigh.edu/~spear/courses.json'); // list of strings
-  //var url = Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json');      // list of maps
-  var url =
-      Uri.parse('https://jsonplaceholder.typicode.com/albums/1'); // single map
-  var headers = {"Accept": "application/json"}; // <String,String>{};
+class Idea {
+  /// The string representation of the number
+  final String str;
 
-  var response = await http.get(url, headers: headers);
+  const Idea({
+    required this.str,
+  });
 
-  developer.log('Response status: ${response.statusCode}');
-  developer.log('Response headers: ${response.headers}');
-  developer.log('Response body: ${response.body}');
+  factory Idea.fromJson(Map<String, dynamic> json) {
+    return Idea(
+      str: json['title'],
+    );
+  }
+}
 
-  final List<String> returnData;
+Future<List<Idea>> fetchIdeas() async {
+  final response = await http
+      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com/ideas'));
+
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response, then parse the JSON.
+    final List<Idea> returnData;
     var res = jsonDecode(response.body);
+    res = res['mData'];
     print('json decode: $res');
 
     if (res is List) {
-      returnData = (res as List<dynamic>).map((x) => x.toString()).toList();
+      returnData = (res).map((x) => Idea.fromJson(x)).toList();
     } else if (res is Map) {
-      returnData = <String>[(res as Map<String, dynamic>).toString()];
+      returnData = <Idea>[Idea.fromJson(res as Map<String, dynamic>)];
     } else {
       developer
           .log('ERROR: Unexpected json response type (was not a List or Map).');
       returnData = List.empty();
     }
+    return returnData;
   } else {
-    throw Exception(
-        'Failed to retrieve web data (server returned ${response.statusCode})');
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Did not receive success status code from request.');
   }
-
-  return returnData;
 }
 
-// method for trying out a long-running calculation
-Future<List<String>> doSomeLongRunningCalculation() async {
-  //return simpleLongRunningCalculation();
-  return getWebData(); // we'll try this next.
-}
-
-Future<List<String>> simpleLongRunningCalculation() async {
-  await Future.delayed(Duration(seconds: 5)); // wait 5 sec
-  return ['x', 'y', 'z'];
-}
-
-/// Create object from data like: http://www.cse.lehigh.edu/~spear/5k.json
-class NumberWordPair {
+class Messages {
   /// The string representation of the number
   final String str;
 
-  /// The int representation of the number
-  final int num;
-
-  const NumberWordPair({
+  const Messages({
     required this.str,
-    required this.num,
   });
 
-  factory NumberWordPair.fromJson(Map<String, dynamic> json) {
-    return NumberWordPair(
-      str: json['str'],
-      num: json['num'],
+  factory Messages.fromJson(Map<String, dynamic> json) {
+    return Messages(
+      str: json['massage'],
     );
   }
 }
 
-Future<List<NumberWordPair>> fetchNumberWordPairs() async {
-  final response =
-      await http.get(Uri.parse('http://www.cse.lehigh.edu/~spear/5k.json'));
+Future<List<Messages>> fetchMessages() async {
+  final response = await http
+      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com/ideas'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response, then parse the JSON.
-    final List<NumberWordPair> returnData;
+    final List<Messages> returnData;
     var res = jsonDecode(response.body);
+    res = res['mData'];
     print('json decode: $res');
 
     if (res is List) {
-      returnData = (res as List<dynamic>)
-          .map((x) => NumberWordPair.fromJson(x))
-          .toList();
+      returnData = (res).map((x) => Messages.fromJson(x)).toList();
     } else if (res is Map) {
-      returnData = <NumberWordPair>[
-        NumberWordPair.fromJson(res as Map<String, dynamic>)
-      ];
+      returnData = <Messages>[Messages.fromJson(res as Map<String, dynamic>)];
     } else {
       developer
           .log('ERROR: Unexpected json response type (was not a List or Map).');
@@ -266,17 +237,15 @@ class AddanIdea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FloatingActionButton Sample'),
-      ),
-      body: const Center(child: Text('Press the button below!')),
-      // An example of the floating action button.
-      //
-      // https://m3.material.io/components/floating-action-button/specs
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Add your onPressed code here!
+          //This will take me to the page where I can make a post
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddIdeaState()),
+          );
         },
+        tooltip: 'Add an Idea',
         child: const Icon(Icons.add),
       ),
     );
@@ -288,22 +257,15 @@ class AddanIdea extends StatelessWidget {
 //but instead remove the like from the post
 class LikeIdea extends StatelessWidget {
   const LikeIdea({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FloatingActionButton Sample'),
-      ),
-      body: const Center(
-        child: Text('Press the button with a label below!'),
-      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           // Add your onPressed code here!
         },
-        label: const Text('Approve'),
-        icon: const Icon(Icons.thumb_up),
+        label: const Text('Like'),
+        icon: const Icon(Icons.favorite),
         backgroundColor: Colors.pink,
       ),
     );
@@ -320,9 +282,6 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
-      ),
       body: Center(
         child: ElevatedButton(
             child: const Text('Open route'),
@@ -330,7 +289,7 @@ class FirstRoute extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const SecondRoute()),
+                MaterialPageRoute(builder: (context) => const AddIdeaState()),
               );
             }),
       ),
@@ -338,27 +297,51 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
-//The secondRoute class would be the state of the app once you are in the post
+//The AddIdeaState class would be the state of the app once you are in the post
 //and is a test class t see how you can return to the previous state with information
 //that has been saved without changing any of the older information
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
+class AddIdeaState extends StatelessWidget {
+  const AddIdeaState({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+        title: const Text('Create Post'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
+      body: const Center(
+        child: LikeIdea(),
       ),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: myController,
     );
   }
 }
