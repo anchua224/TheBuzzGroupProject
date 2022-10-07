8ca33c5bdadc34aa98fd434e5084499fa1f824a5
Alex <alg223@lehigh.edu>
2022-10-07 16:40:41 -0400

    flutter code pre recorded

--- mobile/flutter/flutter_application_1/lib/main.dart
+++ mobile/flutter/flutter_application_1/lib/main.dart
@@ -5,27 +5,10 @@ import 'dart:developer' as developer;
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-// This is going to be the post method that will post new ideas
-// that will go into the database
-void my_async_post_method() async {
-  var data = {'title': 'My first post'};
-  var resp = await http.post(
-    Uri.parse(''),
-    headers: {'Content-Type': 'application/json; charset=UTF-8'},
-    body: json.encode(data),
-  );
-  print(resp.body);
-}
-
-// This is gping to be the get method that will get the ideas
-// from the database and display them in the app
-void my_async_get_method() async {
-  var url = Uri.https('');
-  var response = await http.get(url, headers: {});
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-}
-
+// To the next person writing flutter code. flutter.io and geeksforgeeks is your
+// best friend. Flutter.io to see sample code on how each individual widget works
+// and geeksforgeeks to see the properties of each widget and the things you can do
+// with them
 void main() {
   runApp(const MyApp());
 }
@@ -39,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Buzz',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const MyHomePage(title: 'The Buzz'),
     );
@@ -63,21 +46,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+  // This method will make it so the bottom navigation bar works and highlights
+  // whatever tab ur supposed to be in
+  void itemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title), centerTitle: true,
+        title: Text(widget.title),
+        centerTitle: true,
       ),
       body: const Center(
         child: ListofIdeas(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.pinkAccent,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.circle),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: itemTapped,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          //This will take me to the page where I can make a post
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddIdeaState()),
+          );
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
@@ -96,33 +112,49 @@ class _ListofIdeasState extends State<ListofIdeas> {
     super.initState();
   }
 
-  void _retry() {
+  void retry() {
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    // boolean baraible stating that the likes icon should be white
+    bool postLiked = false;
     var fb = FutureBuilder<List<Idea>>(
       future: fetchIdeas(),
       builder: (BuildContext context, AsyncSnapshot<List<Idea>> snapshot) {
         Widget child;
         if (snapshot.hasData) {
-          // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
-          // create  listview to show one row per array element of json response
+          // create listview to show one row per array element of json response
           child = ListView.builder(
-              //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
               padding: const EdgeInsets.all(26.0),
               itemCount: snapshot.data!.length,
-              itemBuilder: /*1*/ (context, i) {
+              itemBuilder: (context, i) {
                 return Column(
                   children: <Widget>[
                     ListTile(
-                      title: Text(
-                        snapshot.data![i].str,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      tileColor: const Color.fromARGB(100, 181, 126, 220),
-                    ),
+                        // This is supposed to remove a like
+                        onLongPress: () {
+                          //updateLike()
+                        },
+                        title: Text(
+                          snapshot.data![i].title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(snapshot.data![i].message),
+                        tileColor: const Color.fromARGB(200, 240, 128, 128),
+                        trailing: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              //pass in snapshot.data.id
+                              postLiked = !postLiked;
+                            });
+                          },
+                          child: Icon(
+                            Icons.favorite,
+                            color: postLiked ? Colors.pink : Colors.white,
+                          ),
+                        )),
                     const Divider(height: 8.0),
                   ],
                 );
@@ -130,8 +162,6 @@ class _ListofIdeasState extends State<ListofIdeas> {
         } else if (snapshot.hasError) {
           child = Text('${snapshot.error}');
         } else {
-          // awaiting snapshot data, return simple text widget
-          // child = Text('Calculating answer...');
           child = const CircularProgressIndicator(); //show a loading spinner.
         }
         return child;
@@ -141,25 +171,25 @@ class _ListofIdeasState extends State<ListofIdeas> {
   }
 }
 
+// Idea class holds the title and message of every idea
 class Idea {
-  /// The string representation of the number
-  final String str;
+  final String title;
+  final String message;
 
-  const Idea({
-    required this.str,
-  });
+  const Idea({required this.title, required this.message});
 
   factory Idea.fromJson(Map<String, dynamic> json) {
     return Idea(
-      str: json['title'],
+      title: json['title'],
+      message: json['massage'],
     );
   }
 }
 
+// fetchIdeas cast is in charge of pulling the title and message from the database
 Future<List<Idea>> fetchIdeas() async {
   final response = await http
-      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com/ideas'));
-
+      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com//ideas'));
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response, then parse the JSON.
     final List<Idea> returnData;
@@ -184,36 +214,36 @@ Future<List<Idea>> fetchIdeas() async {
   }
 }
 
-class Messages {
-  /// The string representation of the number
-  final String str;
+// Likes class holds the num of likes of an individual idea
+class Likes {
+  final int numLikes;
 
-  const Messages({
-    required this.str,
+  const Likes({
+    required this.numLikes,
   });
 
-  factory Messages.fromJson(Map<String, dynamic> json) {
-    return Messages(
-      str: json['massage'],
+  factory Likes.fromJson(Map<int, dynamic> json) {
+    return Likes(
+      numLikes: json['id'],
     );
   }
 }
 
-Future<List<Messages>> fetchMessages() async {
+// fetchLikes method is supposed to fetch the like # for a specific id
+Future<List<Likes>> fetchLikes(int id) async {
   final response = await http
-      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com/ideas'));
-
+      .get(Uri.parse('https://cse216-fl22-team14.herokuapp.com/likes/:id'));
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response, then parse the JSON.
-    final List<Messages> returnData;
+    final List<Likes> returnData;
     var res = jsonDecode(response.body);
     res = res['mData'];
     print('json decode: $res');
 
     if (res is List) {
-      returnData = (res).map((x) => Messages.fromJson(x)).toList();
+      returnData = (res).map((x) => Likes.fromJson(x)).toList();
     } else if (res is Map) {
-      returnData = <Messages>[Messages.fromJson(res as Map<String, dynamic>)];
+      returnData = <Likes>[Likes.fromJson(res as Map<int, dynamic>)];
     } else {
       developer
           .log('ERROR: Unexpected json response type (was not a List or Map).');
@@ -227,79 +257,27 @@ Future<List<Messages>> fetchMessages() async {
   }
 }
 
-//The concept of this class would be used to add an idea to the app.
-//The button for it is present and clickable but I need to be able to add
-//an action as soon as the button is pressed that would take you to another
-//page which will maove you to a different state
-class AddanIdea extends StatelessWidget {
-  const AddanIdea({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //This will take me to the page where I can make a post
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddIdeaState()),
-          );
-        },
-        tooltip: 'Add an Idea',
-        child: const Icon(Icons.add),
-      ),
-    );
+// updateLike method will post the like to the specific post with the specific id
+Future<Likes> updateLike(int id) async {
+  final response = await http.put(
+      Uri.parse('https://cse216-fl22-team14.herokuapp.com/likes/:id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, int>{'id': id}));
+  if (response.statusCode == 200) {
+    // If the server did return a 200 CREATED response,
+    // then parse the JSON.
+    return Likes.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to like post.');
   }
 }
 
-//The class LikeIdea would be used to like an idea when you are currently
-//viewing that post. Once the button is clicked it would be clickable again
-//but instead remove the like from the post
-class LikeIdea extends StatelessWidget {
-  const LikeIdea({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        label: const Text('Like'),
-        icon: const Icon(Icons.favorite),
-        backgroundColor: Colors.pink,
-      ),
-    );
-  }
-}
-
-//The class FirstRoute is just a test class for testing whether I am able
-//to move between states using widgets. THe concept of this would be used
-//to be able to clikc on a post and take you to a state where you can directly
-//read the entire post and actions that can be taken from there
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            child: const Text('Open route'),
-            // Within the `FirstRoute` widget
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AddIdeaState()),
-              );
-            }),
-      ),
-    );
-  }
-}
-
-//The AddIdeaState class would be the state of the app once you are in the post
-//and is a test class t see how you can return to the previous state with information
-//that has been saved without changing any of the older information
+// AddIdeaState class would be the state of the app once you are trying to post
+// an idea
 class AddIdeaState extends StatelessWidget {
   const AddIdeaState({super.key});
 
@@ -309,39 +287,87 @@ class AddIdeaState extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Create Post'),
       ),
-      body: const Center(
-        child: LikeIdea(),
+      body: Column(
+        children: const <Widget>[TextBox()],
       ),
     );
   }
 }
 
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  State<MyCustomForm> createState() => _MyCustomFormState();
+// createPost method is the method called in order to post an idea to the
+// database
+createPost(String title, String message) async {
+  final response = await http.post(
+    Uri.parse('https://cse216-fl22-team14.herokuapp.com/ideas'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'mTitle': title,
+      'mMessage': message,
+    }),
+  );
+  if (response.statusCode == 200) {
+    // If the server did return a 200 CREATED response,
+    // then parse the JSON.
+  } else {
+    // If the server did not return a 200 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to create Post.');
+  }
 }
 
-// Define a corresponding State class.
-// This class holds the data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
+class TextBox extends StatefulWidget {
+  const TextBox({super.key});
+
+  @override
+  State<TextBox> createState() => _TextBoxState();
+}
+
+class _TextBoxState extends State<TextBox> {
   // Create a text controller and use it to retrieve the current value
   // of the TextField.
-  final myController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // The field in which users can text
+  final titleController = TextEditingController();
+  final messageController = TextEditingController();
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
-    myController.dispose();
+    titleController.dispose();
+    messageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: myController,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: titleController,
+          decoration: const InputDecoration(
+              hintText: 'Enter Title', border: OutlineInputBorder()),
+          autofocus: true,
+        ),
+        TextField(
+          controller: messageController,
+          decoration: const InputDecoration(
+              hintText: 'Enter Message', border: OutlineInputBorder()),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                createPost(titleController.text, messageController.text);
+              });
+            },
+            child: const Text('Create Post'))
+      ],
     );
   }
 }
